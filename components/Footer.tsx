83ea7119db032b6bf647e44d85ae9528c8764671
anthev <cameronanthev@gmail.com>
2025-10-11 21:27:13 +1000

    Remove currency selector from footer
    - Removed AUD/USD/EUR/GBP dropdown from footer
    - Kept Secure Payments badge
    - Cleaner footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -96,21 +96,11 @@ export default function Footer() {
           <div className="text-sm text-gray-400 mb-4 md:mb-0">
             © 2025 Community Pledges. All rights reserved.
           </div>
-          <div className="flex items-center space-x-6">
-            <div className="flex items-center space-x-2">
-              <select className="bg-transparent text-gray-400 text-sm border-none outline-none">
-                <option value="AUD">AUD</option>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-                <option value="GBP">GBP</option>
-              </select>
-            </div>
-            <div className="flex items-center space-x-2 text-sm text-gray-400">
-              <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-              </svg>
-              <span>Secure Payments</span>
-            </div>
+          <div className="flex items-center space-x-2 text-sm text-gray-400">
+            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+              <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+            </svg>
+            <span>Secure Payments</span>
           </div>
         </div>
       </div>
